0c2757ae4516831ae3f87e96b7306759a729b0a6
Guillaume <akaiporo@gmail.com>
2017-04-18 15:44:37 +0200

    Update the person test

--- src/metier/test/TestPerson.java
+++ src/metier/test/TestPerson.java
@@ -8,82 +8,89 @@ import metier.Person;
 
 public class TestPerson {
 
-	public Person initPerson(){
-		return new Person("Gerard", "Majax", "0645537031", "g.majax@gmail.com");
-	}
+	//Creation of an inner class "ConcretePerson" to test the abstract class "person"
+	public class ConretePerson extends Person{
+		public ConretePerson(String string, String string2, String string3, String string4) {
+			super(string, string2, string3, string4);
+		}
+
+		public ConretePerson initPerson(){
+			return new ConretePerson("Gerard", "Majax", "0645537031", "g.majax@gmail.com");
+		}
+		
+		@Test(expected=IllegalArgumentException.class)
+		public void testPerson_Name_isEmpty() {
+			new ConretePerson("", "Majax", "0645537031", "g.majax@gmail.com");
+		}
+		@Test(expected=NullPointerException.class)
+		public void testPerson_Name_isNull() {
+			new ConretePerson(null, "Majax", "0645537031", "g.majax@gmail.com");
+		}
+		@Test(expected=IllegalArgumentException.class)
+		public void testPerson_FirstName_isEmpty() {
+			new ConretePerson("Gerard", "", "0645537031", "g.majax@gmail.com");
+		}
+		@Test(expected=NullPointerException.class)
+		public void testPerson_FirstName_isNull() {
+			new ConretePerson("Gerard", null, "0645537031", "g.majax@gmail.com");
+		}
+		@Test(expected=IllegalArgumentException.class)
+		public void testPerson_Phone_HasInvalidLength() {
+			new ConretePerson("Gerard", "Majax", "0644565537031", "g.majax@gmail.com");
+		}
+		@Test(expected=NullPointerException.class)
+		public void testPerson_Phone_isNull() {
+			new ConretePerson("Gerard", "Majax", null, "g.majax@gmail.com");
+		}
+		@Test(expected=IllegalArgumentException.class)
+		public void testPerson_Email_isInvalid() {
+			new ConretePerson("Gerard", "Majax", "0645537031", "g.majax@gmailcom");
+		}
+		@Test(expected=NullPointerException.class)
+		public void testPerson_Email_isNull() {
+			new ConretePerson("Gerard", "Majax", "0645537031", null);
+		}
+		@Test
+		public void testGetId() {
+			ConretePerson tested = initPerson();
+			tested.setId(1);
+			assertEquals(1, tested.getId());
+		}
 	
-	@Test(expected=IllegalArgumentException.class)
-	public void testPerson_Name_isEmpty() {
-		new Person("", "Majax", "0645537031", "g.majax@gmail.com");
-	}
-	@Test(expected=NullPointerException.class)
-	public void testPerson_Name_isNull() {
-		new Person(null, "Majax", "0645537031", "g.majax@gmail.com");
-	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testPerson_FirstName_isEmpty() {
-		new Person("Gerard", "", "0645537031", "g.majax@gmail.com");
-	}
-	@Test(expected=NullPointerException.class)
-	public void testPerson_FirstName_isNull() {
-		new Person("Gerard", null, "0645537031", "g.majax@gmail.com");
-	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testPerson_Phone_HasInvalidLength() {
-		new Person("Gerard", "Majax", "0644565537031", "g.majax@gmail.com");
-	}
-	@Test(expected=NullPointerException.class)
-	public void testPerson_Phone_isNull() {
-		new Person("Gerard", "Majax", null, "g.majax@gmail.com");
-	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testPerson_Email_isInvalid() {
-		new Person("Gerard", "Majax", "0645537031", "g.majax@gmailcom");
-	}
-	@Test(expected=NullPointerException.class)
-	public void testPerson_Email_isNull() {
-		new Person("Gerard", "Majax", "0645537031", null);
-	}
-	@Test
-	public void testGetId() {
-		Person tested = initPerson();
-		tested.setId(1);
-		assertEquals(1, tested.getId());
-	}
-
-	@Test
-	public void testSetId() {
-		Person tested = initPerson();
-		tested.setId(1);
-	}
-	@Test(expected=IllegalArgumentException.class)
-	public void testSetId_isInvalid() {
-		Person tested = initPerson();
-		tested.setId(-1);
-	}
-
-	@Test
-	public void testGetName() {
-		Person tested = initPerson();
-		assertEquals("Gerard", tested.getName());
-	}
-
-	@Test
-	public void testGetFirstName() {
-		Person tested = initPerson();
-		assertEquals("Majax", tested.getFirstName());
-	}
-
-	@Test
-	public void testGetPhoneNumber() {
-		Person tested = initPerson();
-		assertEquals("0645537031", tested.getPhoneNumber());
-	}
-
-	@Test
-	public void testGetEmail() {
-		Person tested = initPerson();
-		assertEquals("g.majax@gmail.com", tested.getEmail());
+		@Test
+		public void testSetId() {
+			ConretePerson tested = initPerson();
+			tested.setId(1);
+		}
+		@Test(expected=IllegalArgumentException.class)
+		public void testSetId_isInvalid() {
+			ConretePerson tested = initPerson();
+			tested.setId(-1);
+		}
+	
+		@Test
+		public void testGetName() {
+			ConretePerson tested = initPerson();
+			assertEquals("Gerard", tested.getName());
+		}
+	
+		@Test
+		public void testGetFirstName() {
+			ConretePerson tested = initPerson();
+			assertEquals("Majax", tested.getFirstName());
+		}
+	
+		@Test
+		public void testGetPhoneNumber() {
+			ConretePerson tested = initPerson();
+			assertEquals("0645537031", tested.getPhoneNumber());
+		}
+	
+		@Test
+		public void testGetEmail() {
+			ConretePerson tested = initPerson();
+			assertEquals("g.majax@gmail.com", tested.getEmail());
+		}
 	}
 
 }
